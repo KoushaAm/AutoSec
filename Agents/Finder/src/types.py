f9cef41a8083b4715fb449abf1e78a1e128a8e5a
Vinci-Velasco <vincivelasco@gmail.com>
2026-01-18 11:17:37 -0800

    finder: added new finder output type class that will be used for autosecstate

---
+++ Agents/Finder/src/types.py
@@ -0,0 +1,23 @@
+# Contains types for other agents to use for the pipeline
+
+from typing import TypedDict, List
+
+class TraceStep(TypedDict, total=False):
+    uri: str
+    line: int
+    message: str # the line of code itself
+
+
+# One trace/flow has multiple steps (lines)
+Trace = List[TraceStep]
+
+
+# A vulnerability instance can have mutliple flows/traces
+class VulnerabilityInstance(TypedDict):
+    traces: List[Trace]
+
+
+class FinderOutput(TypedDict):
+    cwe: str                     # e.g. "cwe-078"
+    language: str                # e.g. "python"
+    vulnerabilities: List[VulnerabilityInstance]
